1b7f31b68bd717d474b5272feb6aaabd72689fb5
Mark van de Korput <dr.theman@gmail.com>
2016-05-05 12:34:26 +0200

    use ofxRemoteUI for settings

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,28 +1,41 @@
 #include "ofApp.h"
 
-#include "ofxXmlSettings.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetVerticalSync(true);
     ofSetFrameRate(60);
-    
     ofSetLogLevel(OF_LOG_VERBOSE);
-    loadSettings();
+    setupParams();
 }
 
-void ofApp::loadSettings(){
-    ofxXmlSettings xml;
-    xml.load("settings.xml");
+void ofApp::setupParams(){
+    RUI_SETUP(); //start RemoteUI server
+    ofAddListener(OFX_REMOTEUI_SERVER_GET_CLIENT_OF_EVENT(), this, &ofApp::ruiServerCallback);
 
+    RUI_NEW_GROUP("NEURON");
+    RUI_SHARE_PARAM(neuron_ip);
+    RUI_SHARE_PARAM(neuron_port, 5001, 65535);
+    //load values from XML, as they were last saved (if they were)
+    RUI_LOAD_FROM_XML();
+    
+    bParamsDirty = true;
+}
+
+void ofApp::applyParams(){
     if(neuron.isConnected())
         neuron.disconnect();
 
-    neuron.connect(xml.getValue("app:neuron:ip", "127.0.0.1"), xml.getValue("app:neuron:port", 7001));
+    neuron.connect(neuron_ip, neuron_port);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    if(bParamsDirty){
+        applyParams();
+        bParamsDirty = false;
+    }
+
     neuron.update();
 }
 
@@ -36,58 +49,15 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-    if(key == 'l'){
-        loadSettings();
+void ofApp::ruiServerCallback(RemoteUIServerCallBackArg &arg){
+    
+    switch (arg.action) {
+        case CLIENT_DID_SET_PRESET:
+        case CLIENT_UPDATED_PARAM:
+            bParamsDirty = true;
+            break;
+        default:
+            break;
     }
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,27 +2,36 @@
 
 #include "ofMain.h"
 #include "ofxPerceptionNeuron.h"
+#include "ofxRemoteUIServer.h"
 
 class ofApp : public ofBaseApp{
 	public:
 		void setup();
 		void update();
 		void draw();
+//        void exit();
 		
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+//		void keyPressed(int key);
+//		void keyReleased(int key);
+//		void mouseMoved(int x, int y);
+//		void mouseDragged(int x, int y, int button);
+//		void mousePressed(int x, int y, int button);
+//		void mouseReleased(int x, int y, int button);
+//		void mouseEntered(int x, int y);
+//		void mouseExited(int x, int y);
+//		void windowResized(int w, int h);
+//		void dragEvent(ofDragInfo dragInfo);
+//		void gotMessage(ofMessage msg);
 
     private:
-        void loadSettings();
+        void setupParams();
+        void applyParams();
+        void ruiServerCallback(RemoteUIServerCallBackArg &arg);
+        bool bParamsDirty;
+
+    private: // params
+        int neuron_port;
+        string neuron_ip;
 
     private:
         ofxPerceptionNeuron::DataReader neuron;
